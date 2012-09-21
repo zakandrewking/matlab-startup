9bb9b84db5f5f81f6016f790edc27984a3d57005
Zak King <zaking17@gmail.com>
2012-09-21 11:23:17 -0700

    updated maxYield to run all outward exchanges

---
+++ dhRxnList.m
@@ -0,0 +1,38 @@
+function list = dhRxnList(size)
+
+switch size
+  case 31
+    list = {'3OAR100'
+              '3OAR40'
+              '3OAR60'
+              '3OAR80'
+              'AGPR'
+              'AKGDH'
+              'ASAD'
+              'DHDPRy'
+              'EAR40x'
+              'EAR60x'
+              'EAR80x'
+              'G6PDH2r'
+              'GAPD'
+              'GLUDy'
+              'GND'
+              'HPYRRx'
+              'HSDy'
+              'ICDHyr'
+              'IPMD'
+              'KARA1'
+              'KARA2'
+              'MDH'
+              'MTHFD'
+              'NADH16pp'
+              'PDH'
+              'PGCD'
+              'SHK3Dr'
+              'TRSARr'
+              'ME1'
+              'LDH_D'
+              'LCARR'};
+end
+
+end
--- startup.m
+++ startup.m
@@ -6,6 +6,7 @@ else
     cd('C:\Documents and Settings\z1king\My Documents\Dropbox\lab');
     addpath('C:\Documents and Settings\z1king\My Documents\Dropbox\git\optSwap');
     addpath('C:\Documents and Settings\z1king\My Documents\Dropbox\git\optSwap\helper');
+    addpath('C:\Documents and Settings\z1king\My Documents\Dropbox\git\optSwap\max-yield\');
 end
 
 setupCobraSolver;
