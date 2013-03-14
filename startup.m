0c3e1c04403bb3c8395e739df66c8669f4dc2354
Zak King <zaking17@gmail.com>
2013-03-14 11:19:17 -0700

    updated to startup.m for rBioNet

--- startup.m
+++ startup.m
@@ -1,7 +1,10 @@
 % check if we're on mac or pc
 if isunix
     cd('~/Dropbox/lab/');
-    addpath_recurse('~/Dropbox/git',{'.svn','.git'});
+    addpath_recurse('~/Dropbox/repos/matlab-startup',{'.svn','.git'});
+    addpath_recurse('~/Dropbox/git/cobra',{'.svn','.git'});
+    addpath_recurse('~/Dropbox/repos/optSwap',{'.svn','.git'});
+    % addpath_recurse('~/lab/software/rBioNet')
     % libsbml
     addpath('/usr/local/lib/')
 else
